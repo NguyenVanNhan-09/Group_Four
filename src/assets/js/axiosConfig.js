63233dfe648a40307a2bf294bbf5007f09f4d62a
TruongDQ <daoquangtruong151097@gmail.com>
2024-10-25 07:56:40 +0700

    signin and signup

---
+++ src/assets/js/axiosConfig.js
@@ -0,0 +1,25 @@
+const API_URL = 'https://08fb-14-224-128-247.ngrok-free.app/'
+
+const apiClient = axios.create({
+    baseURL: API_URL,
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+
+// Bộ đánh chặn (interceptors)
+apiClient.interceptors.request.use(
+    function (config) {
+        const accessToken = localStorage.getItem('accessToken')
+        if (accessToken) {
+            config.headers['Authorization'] = `Bearer ${accessToken}`
+        }
+        return config
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+)
+
+export default apiClient
